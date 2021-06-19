7a7b70800044cd289404b2e7b1305f1b850a3834
vivekanandpv <vivekanandpv@outlook.com>
2021-06-19 16:20:42 +0530

    done

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 const Sample = () => {
+  const [index, setIndex] = useState(0);
+  const [conversionFactor, setConversionFactor] = useState(1);
+
+  const increment = () => {
+    console.log('increment is executing');
+    setIndex((v) => v + 1);
+  };
+
+  useEffect(() => {
+    console.log('useEffect');
+    setConversionFactor((v) => {
+      if (index < 10) {
+        return 1;
+      } else {
+        return 2;
+      }
+    });
+  }, [index]);
+
+  const expensiveFunction = (i) => {
+    console.log('expensiveFunction is executing');
+    return i * i * i;
+  };
+
+  const memoizedValue = useMemo(
+    () => expensiveFunction(conversionFactor),
+    [conversionFactor]
+  );
+
+  //  If it is not memoized, then the expensiveFunction keeps on executing...
+  //  const nonMemoizedValue = expensiveFunction(conversionFactor);
+
   return (
     <>
-      <h3>Sample Component</h3>
+      <h3>
+        Sample Component: Index: {index}; Cube: {memoizedValue}
+      </h3>
+      <hr />
+
+      <button onClick={increment}>Increment</button>
     </>
   );
 };
