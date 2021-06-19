349d7ecedb7b96f14a5892607d7715488674577b
vivekanandpv <vivekanandpv@outlook.com>
2021-06-19 14:12:53 +0530

    done

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -2,26 +2,24 @@ import React, { useEffect, useState } from 'react';
 
 const Sample = (props) => {
   const [counter, setCounter] = useState(0);
-
-  //  state change or props change in this case
-  //  (because of the implementation of useEffect here),
-  //  causes the effect re-run
   const increment = () => {
     setCounter((v) => {
       return v + 1;
     });
   };
 
-  //  emulating componentDidUpdate for state of props change
+  //  Specifying the filtration criterion for the useEffect
   useEffect(() => {
-    console.log(
-      'useEffect: emulating componentDidUpdate : state or prop change'
-    );
-  });
+    console.log('useEffect: for counter change', counter);
+    //    stay away from state changes within the useEffect
+    //    it may turn out to be recursive
+  }, [counter]); //  runs only for counter updates (props change does not matter)
 
   return (
     <>
-      <h3>Sample Component: {counter}</h3>
+      <h3>
+        Sample Component: Counter: {counter}; Index: {props.index}
+      </h3>
       <hr />
       <button onClick={increment}>Increment</button>
     </>
