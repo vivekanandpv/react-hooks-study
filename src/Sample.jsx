35ed51606d022cfe86647cbcdb41c2e066743fd2
vivekanandpv <vivekanandpv@outlook.com>
2021-06-19 12:43:56 +0530

    done

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Sample = () => {
+  const [counter, setCounter] = useState(0);
+
+  //  state change in this case
+  //  (because of the implementation of useEffect here),
+  //  doesn't cause the effect re-run
+  const increment = () => {
+    setCounter((v) => {
+      return v + 1;
+    });
+  };
+
+  //  useEffect must be imported
+
+  //  emulating componentDidMount
+  useEffect(() => {
+    console.log('useEffect: emulating componentDidMount');
+  }, []);
+
   return (
     <>
-      <h3>Sample Component</h3>
+      <h3>Sample Component: {counter}</h3>
+      <hr />
+      <button onClick={increment}>Increment</button>
     </>
   );
 };
