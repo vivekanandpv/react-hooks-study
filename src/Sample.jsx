56d3bc82436af1164844499b766dc4f6ea5b7a9c
vivekanandpv <vivekanandpv@outlook.com>
2021-06-19 12:09:59 +0530

    done

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Sample = () => {
+  //  useState introduces the state variable and a mutator of the state variable
+  //  importing useState is required
+  const [name, setName] = useState('Default Name');
+  //  name is state variable, setName is the dispatcher or mutator for the name
+
+  //  Always remember, function components DO NOT HAVE this!
+  //  name is bound by using {name}
   return (
     <>
       <h3>Sample Component</h3>
+      <p>Name: {name}</p>
     </>
   );
 };
